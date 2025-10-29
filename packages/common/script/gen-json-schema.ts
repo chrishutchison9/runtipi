e376fa10ae9ca7b84da286e48c27816b8d5f5f2b
Nicolas Meienberger <github@thisprops.com>
2025-10-29 19:02:48 +0100

    feat(common): versionned schema versions

--- packages/common/script/gen-json-schema.ts
+++ packages/common/script/gen-json-schema.ts
@@ -3,14 +3,25 @@ import { dynamicComposeUnion } from '../src/schemas/dynamic-compose.js';
 import fs from 'node:fs/promises';
 import { appInfoSchema } from '../src/schemas/app-info.js';
 import { dynamicComposeSchemaArk } from '../src/schemas/dynamic-compose-ark.js';
+import { dynamicComposeSchemaV1 } from '../src/schemas/utils/converters/v1.js';
 
-const dynamicCompose = z.toJSONSchema(dynamicComposeUnion, {});
-const dynamicComposeArk = dynamicComposeSchemaArk.toJsonSchema({});
+const dynamicCompose = z.toJSONSchema(dynamicComposeUnion, { unrepresentable: 'any' });
 const appInfo = z.toJSONSchema(appInfoSchema, { unrepresentable: 'any' });
 
 const outDir = './json-schemas';
 
+const v1 = z.toJSONSchema(dynamicComposeSchemaV1, { unrepresentable: 'any' });
+const v2 = dynamicComposeSchemaArk.toJsonSchema({});
+
 await fs.mkdir(outDir, { recursive: true });
+await fs.mkdir(`${outDir}/v1`, { recursive: true });
+await fs.mkdir(`${outDir}/v2`, { recursive: true });
+
 await fs.writeFile(`${outDir}/dynamic-compose.json`, JSON.stringify(dynamicCompose, null, 2));
-await fs.writeFile(`${outDir}/dynamic-compose-ark.json`, JSON.stringify(dynamicComposeArk, null, 2));
 await fs.writeFile(`${outDir}/app-info.json`, JSON.stringify(appInfo, null, 2));
+
+await fs.writeFile(`${outDir}/v1/dynamic-compose.json`, JSON.stringify(v1, null, 2));
+await fs.writeFile(`${outDir}/v1/app-info.json`, JSON.stringify(appInfo, null, 2));
+
+await fs.writeFile(`${outDir}/v2/dynamic-compose.json`, JSON.stringify(v2, null, 2));
+await fs.writeFile(`${outDir}/v2/app-info.json`, JSON.stringify(appInfo, null, 2));
